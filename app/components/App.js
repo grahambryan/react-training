fc02d455609b15fdf76357218a1c55a6137cc8bc
Graham Bryan <grahambryan@users.noreply.github.com>
2017-07-21 10:39:23 -0500

    adding routing and nav component

--- app/components/App.js
+++ app/components/App.js
@@ -1,21 +1,38 @@
-var React = require('react')
+var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 //Created components 
 var Popular = require('./Popular')
+var Nav = require('./nav')
+var Home = require('./Home')
+var Battle = require('./Battle')
+
 
 // state
 // lifecycle event - shown to screen removed from screen ... etc
 // UI of the component - required 
 
 class App extends React.Component {
-	render() {
-		
-		return (
-			<div className= 'container'>
-				<Popular />
-			</div>
-		)
-	}
+  render() {		
+  	return (
+  	  <Router>
+	  	<div className= 'container'>
+	  	  <Nav />
+	  	    <Switch>
+		  	  <Route exact path='/' component={Home} />
+		  	  <Route path='/Battle' component={Battle} />	
+		  	  <Route path='/popular' component={Popular} />	
+		  	  <Route render={function () {
+		  	  	return <p>Not Found</p> //for path that is not one of the above
+		  	  }} /> 
+	  	    </Switch>  
+	  	</div>
+	  </Router>
+	)
+  }
 }
 
 module.exports = App;
